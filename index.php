26d4fb61ed3fb6cecf32392ddfcdcf04841fe0b1
NaufalFiraas <naufalfiraas09@gmail.com>
2022-04-01 17:48:28 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, minus doloremque voluptates accusamus dolorem placeat, amet consequuntur dignissimos pariatur officia reprehenderit officiis repellendus soluta exercitationem nemo excepturi error quos? Voluptate quae delectus cum ratione quidem aliquid officiis ipsum porro fuga dicta temporibus recusandae laudantium dolores magni, obcaecati eaque doloribus quibusdam!</p>
 </body>
 </html>
